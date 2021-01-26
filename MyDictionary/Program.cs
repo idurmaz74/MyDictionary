2ce703e38ea3ca3b75a0e7591b8092c9513d74d2
ibrahim DURMAZ <idurmaz@yahoo.com>
2021-01-26 22:37:05 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            
+            myDictionary.Add(1, "Adana");
+            myDictionary.Add(2, "Adıyaman");
+        }
+    }
+}
